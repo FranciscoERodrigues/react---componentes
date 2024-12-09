f8f162f05ea62f6089bfdbbc80785ae2c04203e3
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2024-12-09 20:18:15 -0300

    Add files via upload

---
+++ meuprojeto/src/App.jsx
@@ -0,0 +1,34 @@
+//criar um componete
+
+/*
+1° Parte - Import's
+2° Parte - função
+3° Parte - Export
+*/
+
+function app() {
+
+  return(
+
+    <>
+        <h1> Hello Word !!!! </h1>
+              <p>São Paulo não tem agua.</p>
+
+                <h2> lista de compras</h2>
+
+    <ol> 
+    
+              <li> Agua </li>
+              <li> Arroz </li>
+              <li> Farinha</li>
+
+    </ol>
+      
+      <img src="img/e546dc211e382b9c7cb9e805f47ed2c6.jpg" alt="" />
+
+    </>
+  )
+  
+}
+
+export default app
